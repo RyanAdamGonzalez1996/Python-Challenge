c9d6366fdcefe77e8806661fd019f0b1a1785dd3
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-09-23 21:25:57 -0400

    Wrote Psuedo Code for both of the challenges. Will return to this and expand and start researching on the best ways to excute the logic

---
+++ PyBank/main.py
@@ -0,0 +1,45 @@
+import os
+import csv
+
+dates = []
+profits = []
+numberMonths = 0
+totalProfit = 0
+maxProfit = 0
+maxLoss = 0
+maxProfitMonth = ""
+maxLossMonth = ""
+
+#loop
+#Count how many rows in dataset
+#Equal to the number of rows
+
+#Tick up a counter each time it goes through each row during loop
+#numberMonths =+ 1
+
+#Check if the current row in loop has highest profit
+#if .index(1) < maxProfit
+#   maxProfit = .index(1)
+#   maxProfitMonth = .index(0)
+
+#Check if the current row in loop has the greatest loss
+#if .index(1) < maxLoss
+#   maxLoss = .index(1)
+#   maxLossMonth = .index(0)
+
+
+#Add up .index(1) for each row 
+# (totalProfit = flt(.index(1)) + totalProfit)
+
+
+#loop
+
+#Average of profit loss (after loop)
+#totalProfit/numberMonths
+
+#print out results
+
+#writefile = zip(dates,profits)
+
+#create new file and write new set into it
+#.writerows(writefiles)
---
+++ PyPoll/main.py
@@ -0,0 +1,22 @@
+import os
+import csv
+
+candidateList = []
+#maybe/Dictionary
+#candidateList ={} 
+#"Candidate": votes
+voteTotal = 0
+
+#loop
+
+#each time a row is read in loop, increment vote total
+#voteTotal = voteTotal +=
+
+#Each row, check the candidate, if it's unique; append it to candidateLsit
+#increment a variable/dictionary for each occuronce of candidate
+
+#loop
+
+#print out results
+
+#create new file and write new set into it

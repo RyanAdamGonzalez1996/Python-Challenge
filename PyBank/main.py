a93c5d1b277abd2ccbd65002f5db6c6809a28a93
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-09-27 19:24:35 -0400

    Set up the code to read trhoguh the resources.csv file and retrieve the maxProfit, maxProfitMonth,maxLoss, maxLossMonth, and the total sum of the profit/loss column. Need to get the average change over the period, and to upload this ndata into a csv file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,37 +9,46 @@ maxProfit = 0
 maxLoss = 0
 maxProfitMonth = ""
 maxLossMonth = ""
+inputPath = os.path.join('Resources', 'budget_data.csv')
 
-#loop
-#Count how many rows in dataset
-#Equal to the number of rows
+with open(inputPath, 'r') as csvFile:
 
-#Tick up a counter each time it goes through each row during loop
-#numberMonths =+ 1
+    csvreader = csv.reader(csvFile, delimiter=',')
+    # skips the header
+    next(csvreader)
+    # loop through file
+    for row in csvreader:
+        # Count how many rows in dataset
+        # Equal to the number of rows
+        numberMonths += 1
+        #Sets the current rows second column to an integer
+        row[1] = int(row[1])
+        # Check if the current row in loop has highest profit
+        if row[1] > maxProfit:
+            maxProfit = row[1]
+            maxProfitMonth = row[0]
 
-#Check if the current row in loop has highest profit
-#if .index(1) < maxProfit
-#   maxProfit = .index(1)
-#   maxProfitMonth = .index(0)
+        # Check if the current row in loop has the greatest loss
+        if row[1] < maxLoss:
+            maxLoss = row[1]
+            maxLossMonth = row[0]
 
-#Check if the current row in loop has the greatest loss
-#if .index(1) < maxLoss
-#   maxLoss = .index(1)
-#   maxLossMonth = .index(0)
+        # Add each rows profit to the totalProfit
+        totalProfit = row[1] + totalProfit
 
+    # loop
+print(totalProfit)
+print(maxProfit)
+print(maxProfitMonth)
+print(maxLoss)
+print(maxLossMonth)
 
-#Add up .index(1) for each row 
-# (totalProfit = flt(.index(1)) + totalProfit)
+# Average of profit loss (after loop)
+# totalProfit/numberMonths
 
-
-#loop
-
-#Average of profit loss (after loop)
-#totalProfit/numberMonths
-
-#print out results
+# print out results
 
 #writefile = zip(dates,profits)
 
-#create new file and write new set into it
-#.writerows(writefiles)
+# create new file and write new set into it
+# .writerows(writefiles)

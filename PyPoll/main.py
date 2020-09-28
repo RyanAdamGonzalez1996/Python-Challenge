bd628df9679176ef91330e407f5425535ca8705e
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-09-27 21:24:03 -0400

    Code now has two different lists that reference each other to keep track of each candidate and how amy votes they have

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,9 @@
 import os
 import csv
+import pandas as pd
 
 candidateList = []
+candidateVotes = [0,0,0,0]
 #maybe/Dictionary
 #candidateList ={} 
 #"Candidate": votes
@@ -10,6 +12,7 @@ voteTotal = 0
 #Declare the input and output file paths for easier reading of code
 inputPath = os.path.join('Resources', 'election_data.csv')
 outputPath = os.path.join('Analysis', 'output.txt')
+#df = pd.read_csv('Resources', 'election_data.csv')
 
 with open(inputPath, 'r') as csvFile:
 
@@ -18,16 +21,27 @@ with open(inputPath, 'r') as csvFile:
     next(csvreader)
     #loop through file
     for row in csvreader:
+        #Each row, check the candidate, if it's unique; append it to candidateLsit
         #Only add Unique candidate to Candidate List
         if row[2] not in candidateList:
             candidateList.append(row[2])
-    
+        
+        #Each time loop goes through row = 1 vote
+        voteTotal += 1
+        
+        #Creates a second list that tracks the votes for each candidate
+        #the vote count is the same index as the candidate index
+        for candidate in candidateList:
+            if candidate == row[2]:
+                candidateVotes[candidateList.index(candidate)] += 1
 
 print(candidateList)
+print(candidateVotes)
+print(voteTotal)
 #each time a row is read in loop, increment vote total
 #voteTotal = voteTotal +=
 
-#Each row, check the candidate, if it's unique; append it to candidateLsit
+
 #increment a variable/dictionary for each occuronce of candidate
 
 #loop
